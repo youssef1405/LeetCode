a5c9edc990685dc210b41e02b1d9d8e7a010a7da
Youssef Girgeis <youssef_girgeis@hotmail.com>
2023-09-20 11:03:41 -0400

    solve problem 83 in python

---
+++ Algorithms/Easy/83_remove_duplicates_from_sorted_list.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        curr = head
+        while curr and curr.next:
+            if curr.val == curr.next.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        return head
